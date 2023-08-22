9c416e7ad0d3727eedd72ea163c45e7cb2b08456
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-08-22 15:00:20 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import {useState} from "react";
+import './App.css'
+
 const App = () => {
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrease = () => {
+    setCount(count - 1)
+  }
 
   return (
-    <div>
-      <h1>React App</h1>
-    </div>
+    <section className='sectionCount'>
+      <p className='sectionCount__text'>contagem: {count}</p>
+      <div className='sectionCountBox'>
+        <button className='sectionCountBox__button' onClick={increment}>+</button>
+        <button className='sectionCountBox__button' onClick={decrease}>-</button>
+      </div>
+    </section>
   )
 }
 
